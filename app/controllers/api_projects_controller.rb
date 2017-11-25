8b6e0830635f60a876b8b5c676da8e348ff85db5
raegertay <raegertay89@gmail.com>
2017-11-25 16:18:42 +0800

    Added api to delete project

--- app/controllers/api_projects_controller.rb
+++ app/controllers/api_projects_controller.rb
@@ -5,4 +5,11 @@ class ApiProjectsController < ApplicationController
     render json: @projects.select(:name)
   end
 
+  def destroy
+    @project = Project.find_by(name: params[:name])
+    @project.destroy
+    @projects = Project.all
+    render status: 200, json: @projects
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
 
   scope '/api' do
     get '/projects', to: 'api_projects#index'
-    
+    delete '/project/:name', to: 'api_projects#destroy'
+
     scope '/project' do
       get '/:name', to: 'api_notes#index'
       post '/:name', to: 'api_notes#create'
