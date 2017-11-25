5bbed5f37880970f4be5914cbbbffac5e333abb0
raegertay <raegertay89@gmail.com>
2017-11-25 11:42:41 +0800

    Added api for saving new note

--- app/controllers/api_notes_controller.rb
+++ app/controllers/api_notes_controller.rb
@@ -2,8 +2,15 @@ class ApiNotesController < ApplicationController
 
   def index
     @user = User.find_or_create_by(name: params[:name])
-    @notes = @user.notes
+    @notes = @user.notes.select(:id, :title, :body)
     render json: @notes
+  end
+
+  def create
+    @user = User.find_by(name: params[:name])
+    @user.notes.create(title: params[:title], body: params[:body])
+    @notes = @user.notes.select(:id, :title, :body)
+    render status: 200, json: @notes
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   scope '/api' do
     get '/user/:name', to: 'api_notes#index'
+    post '/user/:name', to: 'api_notes#create'
   end
 
 end
